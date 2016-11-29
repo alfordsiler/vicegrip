bbfc374ada2fe54065617134fc7625d5287f1946
jedimasterclos <carlossrinivas@gmail.com>
2016-11-29 14:39:34 -0800

    Carlos: Going on all sorts of dates

--- db/migrate/20161129172824_create_setbacks.rb
+++ db/migrate/20161129172824_create_setbacks.rb
@@ -1,7 +1,7 @@
 class CreateSetbacks < ActiveRecord::Migration[5.0]
   def change
     create_table :setbacks do |t|
-      t.date :setback_date
+      t.datetime :setback_date
       t.integer :setback_cost
       t.references :user, foreign_key: true
       t.references :vice, foreign_key: true
