496d9486da8bd028eb56296514607839df42f5bb
alfordsiler <alfordsiler@gmail.com>
2016-11-29 09:44:07 -0800

    updated models

--- db/migrate/20161129172733_create_vices.rb
+++ db/migrate/20161129172733_create_vices.rb
@@ -2,9 +2,9 @@ class CreateVices < ActiveRecord::Migration[5.0]
   def change
     create_table :vices do |t|
       t.string :vice_name
-      t.string :vice_cost
+      t.integer :vice_cost
       t.string :goal_name
-      t.string :goal_cost
+      t.integer :goal_cost
       t.references :user, foreign_key: true
 
       t.timestamps
--- db/migrate/20161129172824_create_setbacks.rb
+++ db/migrate/20161129172824_create_setbacks.rb
@@ -1,8 +1,8 @@
 class CreateSetbacks < ActiveRecord::Migration[5.0]
   def change
     create_table :setbacks do |t|
-      t.string :setback_date
-      t.string :setback_cost
+      t.date :setback_date
+      t.integer :setback_cost
       t.references :user, foreign_key: true
       t.references :vice, foreign_key: true
 
